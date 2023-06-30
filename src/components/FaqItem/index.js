43fd05d8209b0b3f8d9414d8427a2b1f482693fe
laxmichelluri <laxmichelluri744@gmail.com>
2023-06-30 07:56:41 +0000

    faqs-app

---
+++ src/components/FaqItem/index.js
@@ -0,0 +1,62 @@
+// Write your code here.
+import {Component} from 'react'
+import './index.css'
+
+const plusImage =
+  'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+const minusImage =
+  'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+
+class FaqItem extends Component {
+  state = {isActive: false}
+
+  onToggleIsActive = () => {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive,
+    }))
+  }
+
+  renderAnswer = () => {
+    const {isActive} = this.state
+    const {faqDetails} = this.props
+    const {answerText} = faqDetails
+
+    if (isActive) {
+      return (
+        <div>
+          <hr className="horizontal-line" />
+          <p className="answer"> {answerText}</p>
+        </div>
+      )
+    }
+    return null
+  }
+
+  renderActiveImage = () => {
+    const {isActive} = this.state
+    const imgUrl = isActive ? minusImage : plusImage
+    const altText = isActive ? 'minus icon' : 'plus icon'
+
+    return (
+      <button type="button" className="button" onClick={this.onToggleIsActive}>
+        <img src={imgUrl} className="image" alt={altText} />
+      </button>
+    )
+  }
+
+  render() {
+    const {faqDetails} = this.props
+    const {questionText} = faqDetails
+
+    return (
+      <li className="faq-item">
+        <div className="question-container">
+          <h1 className="question-heading">{questionText}</h1>
+          {this.renderActiveImage()}
+        </div>
+        {this.renderAnswer()}
+      </li>
+    )
+  }
+}
+export default FaqItem
